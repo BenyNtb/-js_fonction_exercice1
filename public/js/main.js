945761487a8af26c2f182359ce94294a838c1dae
Beny <b.ntambwe97@gmail.com>
2021-01-12 17:34:49 +0100

    almost done

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+// EXO 1
+// let nbr = (a,b) => {
+//     return `${a} + ${b} = ${a+b}`
+// }
+// console.log(nbr(5,5));
+
+// EXO 2
+
+// let ok = (a,b) => {
+//     return `${15} - ${5} = ${a-b}`
+// };
+// console.log((ok(15,5)));
+
+// EXO 3
+
+// let nbr2 = (a,b) => {
+//     return `${a} * ${b} = ${a*b}`
+// }
+// console.log(nbr2(50,5));
+
+// EXO 4
+// let nbr3 = (a,b) => {
+//     return `${a} / ${b} = ${a/b}`
+// }
+// console.log(nbr3(50,5));
+
+// EXO 5
+let nbr4 = (a,b) => {
+    return `${a} % ${b} = ${a%b}`
+}
+console.log(nbr4(25,5));
